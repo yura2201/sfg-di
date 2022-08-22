ab81b01182ab534ca8b525ee384277bc3e81156b
Yuriy Tsarkov <yurait6@gmail.com>
2022-08-22 22:56:07 +0300

    My DI assignment review

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -2,14 +2,21 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
-    private final PetService petService;
+  private final PetService petService;
 
-    public String whichPetIsTheBest(){
-        return petService.getPetType();
-    }
+  public PetController(PetService petService) {
+    this.petService = petService;
+  }
+
+  public String whichPetIsTheBest() {
+    return petService.getPetType();
+  }
 }
