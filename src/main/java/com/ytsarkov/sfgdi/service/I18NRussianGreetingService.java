a221c9dfbb029cd397aab7bdf9b2f50663af7bf2
Yuriy Tsarkov <yurait6@gmail.com>
2022-08-22 19:13:24 +0300

    Example of default profile

--- src/main/java/com/ytsarkov/sfgdi/service/I18NRussianGreetingService.java
+++ src/main/java/com/ytsarkov/sfgdi/service/I18NRussianGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by Yuriy Tsarkov on 22.08.2022
  */
-@Profile("RU")
+@Profile({ "RU", "default" })
 @Service("i18nService")
 public class I18NRussianGreetingService implements GreetingService {
   @Override public String sayGreeting() {
